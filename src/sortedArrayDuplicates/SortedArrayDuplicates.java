0e49f9cbb3913f3a6587eef0cf9be1a58d850325
breethy <752330452@qq.com>
2022-11-13 17:34:44 +0800

    删除有序数组中重复的数，并输出长度

--- src/sortedArrayDuplicates/SortedArrayDuplicates.java
+++ src/sortedArrayDuplicates/SortedArrayDuplicates.java
@@ -1,7 +1,13 @@
 package sortedArrayDuplicates;
 
+import static sortedArrayDuplicates.RemoveDuplicates.removeDuplicates;
+
 /**
  * @author Breethy
+ * @create 2022/11/13 17:18
  */
 public class SortedArrayDuplicates {
+    public static void main(String[] args) {
+        System.out.println(removeDuplicates(new int[]{0,1,2,2,3,3,4}));
+    }
 }
